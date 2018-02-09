b33081dcc21f4da41ae91b3fe3996060813204ff
ft3 <ft3@void.lan>
2018-02-09 19:05:25 +0100

    ex 4.1 - done

--- chapter_4/ex41.c
+++ chapter_4/ex41.c
@@ -1,3 +1,28 @@
 #include <stdio.h>
 
-int strrindex(
+int strrindex(char [], char);
+
+int
+main()
+{
+	printf("%d\n", strrindex("abcd", 'g'));
+	return 0;
+}
+
+int
+strrindex(char s[], char t)
+{
+	/* calculate strlen */
+	int len;
+	len = 0;
+	while (s[len] != '\0')
+		len++;
+	while (s[len] != t && len > 0)
+		len--;
+	if (len > 0)
+		return len;
+	else if (s[0] == t)
+		return 0;
+	else
+		return -1;
+}
