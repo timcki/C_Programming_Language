507dbbc75c9bbd2efdb9d56b9b1132d10040353d
ft3 <f33t@protonmail.com>
2018-02-12 13:39:10 +0100

    ex 4.3 done

--- chapter_4/ex43-10.c
+++ chapter_4/ex43-10.c
@@ -13,8 +13,11 @@ typedef enum {
 	DIVISION,
 	MODULO,
 	RESULT,
+	QUIT,
 	UNKNOWN
 } Optype;
+
+/* -> Basic calculator from exercise extended with enum type for operations. */
 
 
 /* input handling */
@@ -38,20 +41,26 @@ main()
 	char s[MAXOP];
 
 	while ((type = getOp(s)) != EOF) {
+
 		switch (type) {
+
 		case NUMBER:
 			push(atof(s));
 			break;
+
 		case ADDITION:
 			push(pop() + pop());
 			break;
+
 		case MULTIPLICATION:
 			push(pop() * pop());
 			break;
+
 		case SUBSTRACTION:
 			op2 = pop();
 			push(pop() - op2);
 			break;
+
 		case DIVISION:
 			op2 = pop();
 			if (!op2)
@@ -61,12 +70,25 @@ main()
 				return -1;
 			}
 			break;
-		case RESULT:
-			printf("\t%.8f\n", pop());
+
+		case MODULO:
+			op2 = pop();
+			if (!op2)
+				push((int) pop() % (int) op2);
+			else
+				printf("ERROR: Division by zero\n");
 			break;
+
+		case RESULT:
+			printf("\t%.8g\n", pop());
+			break;
+
+		case QUIT:
+			return 0;
+
 		case UNKNOWN:
 			printf("ERROR: Unkown operation: %s\n", type);
-			return -1;
+
 		}
 	}
 	return 0;
@@ -86,6 +108,10 @@ translateOp(char c)
 		return MULTIPLICATION;
 	case '\n': case '=':
 		return RESULT;
+	case '%':
+		return MODULO;
+	case 'q':
+		return QUIT;
 	default:
 		return UNKNOWN;
 	}
@@ -94,26 +120,42 @@ translateOp(char c)
 Optype
 getOp(char s[])
 {
-	int i, c;
+	int i, c, tmp;
+	i = 0;
 	while ((s[0] = c = getch()) == ' ' || c == '\t')
 		;
+
+
 	s[1] = '\0';
 
-	/* return value if operand and not number */
-	if (!isdigit(c) && c != '.')
+	/* return operand if not number or - sign or fraction dot */
+	if (!isdigit(c) && c != '.' && c != '-')
 		return translateOp(c);
-	i = 0;
+	else if (c == '-') {
+		if (isdigit(c = getch())) {
+			s[++i] = c;
+		}
+		else {
+			ungetch(c);
+			return translateOp('-');
+		}
+	}
+	
 	/* integer part */
 	if (isdigit(c))
 		while (isdigit(s[++i] = c = getch()))
 			;
+
 	/* fraction part */
 	if (c == '.')
 		while (isdigit(s[++i] = c = getch()))
 			;
+
+
 	s[i] = '\0';
 	if (c != EOF)
 		ungetch(c);
+
 	return NUMBER;
 }
 
@@ -128,7 +170,7 @@ push(double v)
 	if (sp < MAXVAL)
 		stack[sp++] = v;
 	else
-		printf("ERROR: Stack full can't push %f\n", v);
+		printf("ERROR: Stack full can't push %g\n", v);
 }
 
 double
@@ -146,7 +188,7 @@ pstack(void)
 {
 	printf("STACK(%d):\n", sp);
 	for (int i = 0; i < sp; ++i) {
-		printf("%f, ", stack[sp]);
+		printf("%f, ", stack[i]);
 	}
 	printf("\n");
 }
@@ -168,7 +210,7 @@ void
 ungetch(int c)
 {
 	if (bufp >= BUFSIZE)
-		printf("ERROR: ungetch: too many characters\n");
+		printf("ERROR: ungetch -  too many characters\n");
 	else
 		buf[bufp++] = c;
 }
